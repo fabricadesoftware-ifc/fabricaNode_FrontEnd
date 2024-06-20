4cfcac1cf8eeb8555e362e130361b96f71512341
Caussz <cczcaua@gmail.com>
2024-06-20 14:52:25 -0300

    FEAT: Adding components for graph and painel control #13

---
+++ src/components/graph/GraphContainer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+    nodes: Object,
+    edges: Object,
+    configs: Object,
+});
+</script>
+
+<template>
+    <div class="graph">
+        <v-network-graph :zoom-level="0.5" :nodes="nodes" :edges="edges" :configs="configs" />
+    </div>
+</template>
+
+<style scoped>
+.graph {
+    width: 600px;
+    height: 400px;
+    border: 1px solid #ccc;
+    margin-bottom: 20px;
+}
+</style>
