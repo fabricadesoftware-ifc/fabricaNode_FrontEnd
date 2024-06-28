feec6a79c9f739edf30cd01d49a6640dd3db6360
Caussz <cczcaua@gmail.com>
2024-06-28 08:32:39 -0300

    FEAT: Correcting components and adding the necessary for the issue #25

---
+++ src/components/graph/label/NodeLabel.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { useNodeStore, useTemplateStore } from '@/stores';
+
+const nodeStore = useNodeStore();
+const templateStore = useTemplateStore();
+</script>
+
+<template>
+  <div
+    ref="tooltip"
+    class="tooltip"
+    :style="{
+      opacity: templateStore.tooltipOpacity,
+      position: 'absolute',
+      top: nodeStore.currentNodePosition.y + 'px',
+      left: nodeStore.currentNodePosition.x + 'px',
+    }"
+  >
+    <div>{{ nodeStore.currentNode.node }}</div>
+  </div>
+</template>
+
+<style scoped>
+.tooltip {
+  top: 0;
+  left: 0;
+  opacity: 0;
+  position: absolute;
+  width: 500px;
+  height: auto;
+  display: grid;
+  place-content: center;
+  text-align: center;
+  font-size: 12px;
+  background-color: #fff0bd;
+  border: 1px solid #ffb950;
+  box-shadow: 2px 2px 2px #aaa;
+  transition: opacity 0.2s linear;
+  pointer-events: none;
+}
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import GraphContainer from "./graph/GraphContainer.vue";
+import NodeLabel from "./graph/label/NodeLabel.vue";
 import PainelControl from "./painel/PainelControl.vue";
 
 export {
     GraphContainer,
-    PainelControl
+    PainelControl,
+    NodeLabel
 };
