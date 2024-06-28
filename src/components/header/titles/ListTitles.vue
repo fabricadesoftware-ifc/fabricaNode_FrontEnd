46a5111c91291c3cbf7b7dbdff3e3f43edf72031
Cauã Vinicius <77009854+Caussz@users.noreply.github.com>
2024-06-28 15:03:57 -0300

    Update ListTitles.vue

--- src/components/header/titles/ListTitles.vue
+++ src/components/header/titles/ListTitles.vue
@@ -12,13 +12,6 @@ defineProps({
 </template>
 
 <style scoped>
-.routers-desktop li {
-    font-family: "Montserrat", sans-serif;
-    list-style: none;
-    font-size: 18px;
-    color: black;
-}
-
 .routers-mobile li {
     font-family: "Montserrat", sans-serif;
     list-style: none;
@@ -34,16 +27,5 @@ defineProps({
 
 .router-link {
     text-decoration: none;
-}
-@media (max-width:1000px) {
-    .routers-desktop li {
-        font-size: 16px;
-    }
-    
-}
-@media (max-width:820px) {
-    .routers-desktop li {
-        font-size: 14px;
-    }     
 }
 </style>
