ce93f8fa8dbd93624114fa43117628e6d09a241e
isabellirosa <isabelli.ifc@gmail.com>
2024-08-08 12:23:30 -0300

    FEAT: Adding a favorite boolean field to the mock authors and keywords #50

--- mock/keywords/keywords.ts
+++ mock/keywords/keywords.ts
@@ -1,171 +1,213 @@
 const keywords = [
     {
         id: 1,
-        key: "algoritmos"
+        key: "algoritmos",
+        favorite: false
     },
     {
         id: 2,
-        key: "genéticos"
+        key: "genéticos",
+        favorite: false
     },
     {
         id: 3,
-        key: "otimização"
+        key: "otimização",
+        favorite: false
     },
     {
         id: 4,
-        key: "inteligência artificial"
+        key: "inteligência artificial",
+        favorite: false
     },
     {
         id: 5,
-        key: "machine learning"
+        key: "machine learning",
+        favorite: false
     },
     {
         id: 6,
-        key: "redes neurais"
+        key: "redes neurais",
+        favorite: false
     },
     {
         id: 7,
-        key: "medicina"
+        key: "medicina",
+        favorite: false
     },
     {
         id: 8,
-        key: "diagnóstico"
+        key: "diagnóstico",
+        favorite: false
     },
     {
         id: 9,
-        key: "previsão de mercado"
+        key: "previsão de mercado",
+        favorite: false
     },
     {
         id: 10,
-        key: "finanças"
+        key: "finanças",
+        favorite: false
     },
     {
         id: 11,
-        key: "deep learning"
+        key: "deep learning",
+        favorite: false
     },
     {
         id: 12,
-        key: "processamento de imagem"
+        key: "processamento de imagem",
+        favorite: false
     },
     {
         id: 13,
-        key: "visão computacional"
+        key: "visão computacional",
+        favorite: false
     },
     {
         id: 14,
-        key: "sistemas embarcados"
+        key: "sistemas embarcados",
+        favorite: false
     },
     {
         id: 15,
-        key: "internet das coisas"
+        key: "internet das coisas",
+        favorite: false
     },
     {
         id: 16,
-        key: "IoT"
+        key: "IoT",
+        favorite: false
     },
     {
         id: 17,
-        key: "big data"
+        key: "big data",
+        favorite: false
     },
     {
         id: 18,
-        key: "análise de dados"
+        key: "análise de dados",
+        favorite: false
     },
     {
         id: 19,
-        key: "tempo real"
+        key: "tempo real",
+        favorite: false
     },
     {
         id: 20,
-        key: "blockchain"
+        key: "blockchain",
+        favorite: false
     },
     {
         id: 21,
-        key: "segurança da informação"
+        key: "segurança da informação",
+        favorite: false
     },
     {
         id: 22,
-        key: "criptografia"
+        key: "criptografia",
+        favorite: false
     },
     {
         id: 23,
-        key: "computação quântica"
+        key: "computação quântica",
+        favorite: false
     },
     {
         id: 24,
-        key: "algoritmos quânticos"
+        key: "algoritmos quânticos",
+        favorite: false
     },
     {
         id: 25,
-        key: "futuro da computação"
+        key: "futuro da computação",
+        favorite: false
     },
     {
         id: 26,
-        key: "aprendizado supervisionado"
+        key: "aprendizado supervisionado",
+        favorite: false
     },
     {
         id: 27,
-        key: "classificação"
+        key: "classificação",
+        favorite: false
     },
     {
         id: 28,
-        key: "segurança em redes"
+        key: "segurança em redes",
+        favorite: false
     },
     {
         id: 29,
-        key: "firewall"
+        key: "firewall",
+        favorite: false
     },
     {
         id: 30,
-        key: "detecção de intrusão"
+        key: "detecção de intrusão",
+        favorite: false
     },
     {
         id: 31,
-        key: "smart cities"
+        key: "smart cities",
+        favorite: false
     },
     {
         id: 32,
-        key: "tecnologias emergentes"
+        key: "tecnologias emergentes",
+        favorite: false
     },
     {
         id: 33,
-        key: "sustentabilidade"
+        key: "sustentabilidade",
+        favorite: false
     },
     {
         id: 34,
-        key: "redes de sensores"
+        key: "redes de sensores",
+        favorite: false
     },
     {
         id: 35,
-        key: "comunicação sem fio"
+        key: "comunicação sem fio",
+        favorite: false
     },
     {
         id: 36,
-        key: "agricultura"
+        key: "agricultura",
+        favorite: false
     },
     {
         id: 37,
-        key: "tecnologia agrícola"
+        key: "tecnologia agrícola",
+        favorite: false
     },
     {
         id: 38,
-        key: "protocolo seguro"
+        key: "protocolo seguro",
+        favorite: false
     },
     {
         id: 39,
-        key: "robótica"
+        key: "robótica",
+        favorite: false
     },
     {
         id: 40,
-        key: "energia sustentável"
+        key: "energia sustentável",
+        favorite: false
     },
     {
         id: 41,
-        key: "tecnologias verdes"
+        key: "tecnologias verdes",
+        favorite: false
     },
     {
         id: 42,
-        key: "meio ambiente"
+        key: "meio ambiente",
+        favorite: false
     }
 ];
 
