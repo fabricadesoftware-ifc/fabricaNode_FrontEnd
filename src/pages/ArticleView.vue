68d18b524b32060c6d851be509279cf554754a67
fujulia <juliafu713@gmail.com>
2024-08-23 17:09:34 -0300

    FEAT: Adding filter style #5

--- src/pages/ArticleView.vue
+++ src/pages/ArticleView.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import Magnify from 'vue-material-design-icons/Magnify.vue'
 import BookmarkOutline from 'vue-material-design-icons/BookmarkOutline.vue'
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue'
 import { usePublicationStore } from '@/stores'
 
 const publications = usePublicationStore()
@@ -22,28 +23,46 @@ console.log(publications.entirePublications)
         :key="index"
       >
         <div class="category-line"></div>
-        <h1> <a href="">{{ publication.label }}</a></h1>
+        <h1>
+          <a href="">{{ publication.label }}</a>
+        </h1>
         <div class="info-article">
           Autores:
-          <div v-for="(author, index) in publication.authors" :key="index" class="text-inline"> <a href="">{{ author.name+' ' }}</a></div>
+          <div v-for="(author, index) in publication.authors" :key="index" class="text-inline">
+            <a href="">{{ author.name + ' ' }}</a>
+          </div>
         </div>
         <div class="info-article">
           Palavras-chave:
-          <div v-for="(keyword, index) in publication.keywords" :key="index" class="text-inline text-underline"> <a href="">{{ keyword.key+ ' '+',' }}</a></div>
+          <div
+            v-for="(keyword, index) in publication.keywords"
+            :key="index"
+            class="text-inline text-underline"
+          >
+            <a href="">{{ keyword.key + ' ' + ',' }}</a>
+          </div>
         </div>
         <div class="utility-article">
           <div class="favorite-article">
-            <BookmarkOutline size="20"/> 
+            <BookmarkOutline size="20" />
             <span> Favoritar</span>
           </div>
-          <div>
-            Artigos Relacionados
-          </div>
+          <div>Artigos Relacionados</div>
         </div>
-        <hr>
+        <hr />
       </section>
     </section>
-    <section class="container-filter">mmmm</section>
+    <section class="container-filter">
+      <h3>Filtros</h3>
+      <div class="content-filters">
+        <div>
+          <div>Categoria</div>
+          <div>
+            <ChevronDown />
+          </div>
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 <style scoped>
@@ -64,9 +83,9 @@ console.log(publications.entirePublications)
 .container-filter {
   width: 20vw;
   height: 100vh;
-  background-color: #a7a7a7;
-  position:sticky;
-  top:0;
+  /* background-color: #a7a7a7; */
+  position: sticky;
+  top: 150px;
 }
 
 .container-title div {
@@ -92,7 +111,7 @@ h1 {
   margin-bottom: 10px;
 }
 
-.total-article{
+.total-article {
   font-size: 16px;
   color: #949494;
   font-weight: 600;
@@ -106,45 +125,57 @@ h1 {
 .info-article {
   color: #464343;
   font-weight: 400;
-display:block;
+  display: block;
   margin-bottom: 14px;
 }
 
-.text-inline{
+.text-inline {
   display: inline;
 }
 
-.favorite-article{
+.favorite-article {
   display: flex;
 }
 
- hr{
+hr {
   width: 100%;
   height: 1px;
-  background-color: #C1C1C1;
+  background-color: #c1c1c1;
   margin: 14px 0 14px 0;
 }
 
-.utility-article{
+.utility-article {
   display: flex;
   gap: 27px;
 }
 
-h1 a{
+h1 a {
   color: black;
 }
 
-a{
+a {
   color: #464343;
 }
 
-a:hover{
+a:hover {
   text-decoration: underline;
   transition: 3s;
 }
 
-.text-underline{
+.text-underline {
   text-decoration: underline;
 }
 
+h3 {
+  font-size: 20px;
+  margin: 0 0 25px 25px;
+  font-weight: 600;
+}
+
+.content-filters {
+  height: calc(100% - 50px);
+  width: 100%;
+  border-top: 1px solid #c1c1c1;
+  border-left: 1px solid #c1c1c1;
+}
 </style>
